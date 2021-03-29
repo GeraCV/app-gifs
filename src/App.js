96b0322c17cf6862fb186a5a7471388a773cce61
Gerardo CV <gerardocarrizosavalentin1a@gmail.com>
2021-03-28 20:08:46 -0600

    Pintando los primeros gifs.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [gifs, setGifs] = useState([])
+
+  const getGifs = async () => {
+    const api = `https://api.giphy.com/v1/gifs/search?api_key=mfOwuN9UxxhOso3kVH1zxk4EvttJGAio&q=avengers&limit=5&offset=0&rating=g&lang=en`
+    const response = await fetch(api)
+    const information = await response.json()
+    const { data } = information
+    console.log(data)
+    const gifs = data.map(gif => {
+      const { id, images, title } = gif
+      const url = images.downsized_medium.url
+      return { id, url, title }
+    })
+    return gifs
+  }
+
+  useEffect(() => {
+    getGifs().then(gifs => setGifs(gifs))
+  }, [setGifs])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="principal">
+        <h2 className="title"> Gifs </h2>
+        <div className="container-gifs">
+          {
+            gifs.map(({ title, url, id }) => {
+              return (
+                <div className="container-gif" key={id}>
+                  <img className="image-gif" src={url} alt={title} />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+
+export default App
